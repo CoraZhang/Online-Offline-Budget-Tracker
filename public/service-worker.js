7ab05ff5218bfdd07ad9cc1b288f9421441be295
Shihan Zhang <corazhang0@gmail.com>
2020-11-16 16:42:21 -0500

    update file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,80 +1,76 @@
-
 const CACHE_NAME = "budget-static-cache-v2";
 const DATA_CACHE_NAME = "budget-data-cache-v1";
 
-const iconSizes = ["72", "96", "128", "144", "152", "192", "384", "512"];
+const iconSizes = ["192", "512"];
 const iconFiles = iconSizes.map(
-  (size) => `/icons/icon-${size}x${size}.png`
+    (size) => `/icons/icon-${size}x${size}.png`
 );
 
 const staticFilesToPreCache = [
-  "/",
-  "/index.html",
-  "/index.js",
-  "/db.js",
-  "/style.css",
-  "/favicon.ico",
-  "/manifest.webmanifest",
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/style.css",
+    "/favicon.ico",
+    "/manifest.webmanifest",
 ].concat(iconFiles);
 
 // install
 self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      //console.log("Your files were pre-cached successfully!");
-      return cache.addAll(staticFilesToPreCache);
-    })
-  );
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(staticFilesToPreCache);
+        })
+    );
 
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 // activate
 self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            //console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
-
-  self.clients.claim();
+    );
+    self.clients.claim();
 });
 
 // fetch
 self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/")) {  
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
-    );
-  } else {
-    // respond from static cache, request is not for /api/*
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
-      })
-    );
-  }
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+    } else {
+        // respond from static cache
+        evt.respondWith(
+            caches.open(CACHE_NAME).then(cache => {
+                return cache.match(evt.request).then(response => {
+                    return response || fetch(evt.request);
+                });
+            })
+        );
+    }
 });
